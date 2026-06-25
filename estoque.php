39b35ce624ab431e051f9148a46e1ddf834a0428
Diogo Meireles <didialexandre1017@gmail.com>
2026-06-25 08:48:30 -0300

    Conclusao Lógica do estoque.php

--- estoque.php
+++ estoque.php
@@ -16,13 +16,63 @@
 <h4>Gerenciamento de Estoque</h4>
 <p>Selecione uma peça para registrar uma entrada ou saída do estoque</p>
 
-Selecione a peça: <?php
+Selecione a peça: 
+<form method="post">
+
+<?php
+
 include_once('conexao.php');
 
-$busca = mysqli_query($conexao,"");
+$busca = mysqli_query($conexao,"SELECT * FROM Produto");
+echo "<select name='id_prod'>";
+while($linha=mysqli_fetch_array($busca)){
+echo "<option value='{$linha['id_produto']}'>{$linha['nome']}</option>";
 
+
+
+
+}
+echo "</select>";
 ?>
+<input 
+type="radio" 
+name="opcao" 
+value="Registrar Entrada">
+
+<label>Registrar Entrada</label>
+
+<br>
+
+<input 
+type="radio" 
+name="opcao" 
+value="Registrar Saída">
+
+<label>Registrar Saída</label><br>
+    
+<input type="submit" value="Cadastrar Entrada Ou Saída">
+</form>
+<?php
+include_once('conexao.php');
+if($_POST){
+
+    $id_prod = $_POST['id_prod'];
+    $opcao = $_POST['opcao'];
+
+    if($opcao == 'Registrar Entrada'){
+
+        header("Location: entrada.php?id=$id_prod&opcao=$opcao");
+
+    }else{
+
+        header("Location: saida.php?id=$id_prod&opcao=$opcao");
+
+    }
+
+}
 
     
+?>
+
 </body>
 </html>
